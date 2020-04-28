119b4d459032ed865dd8befbb95b132ed9519a2e
PabloAceG <pabloacereda98@gmail.com>
2020-04-28 16:38:07 +0200

    Parse of dataset complete. Structure to obtain metrics is functional

--- code/metrics.py
+++ code/metrics.py
@@ -27,16 +27,16 @@ with open (dataset_path, 'r') as csv_file:
 def parse_dataset () : 
     ## Data
     # Input 
-    X = numpy.array (dataset) # Transformed to numpy array to allow more operations on it.
-    X = X[:, 0 : -1]
-    X = [ 
+    X = numpy.array (dataset) # Transformed to numpy array to allow more 
+    X = X[:, 0 : -1]          # operations on it.
+    X = numpy.array ( [ 
         numpy.array (row).astype (numpy.float) 
         for row in X 
-    ]
+    ] )
     # Target
-    Y = [
+    Y = numpy.array( [
         row[-1] for row in dataset
-    ]
+    ] )
 
     return X, Y
 
@@ -47,15 +47,22 @@ if __name__ == '__main__':
     #Data
     inputs, target = parse_dataset ()
 
-    print (len(inputs))
-    print (len(inputs[0]))
-    print (len(target))
-
-
+    # R does not take string values. So each class is translated into a 
+    # numerical value.
+    for row in range (len(target)) :
+        if target[row] == 'setosa' :
+            target[row] = 1
+        elif target[row] == 'versicolor' :
+            target[row] = 2
+        elif target[row] == 'virginica' :
+            target[row] = 3
+        else :
+            target[row] = 0
+    
     # Connect to R
     connector = r_connect()
-    # Get metrics for dataset
-    connector.get_metrics(inputs, target)
+    # Compute and print metrics for dataset
+    connector.get_print_metrics(inputs, target)
 
     # TODO: Look at
     # https://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html
